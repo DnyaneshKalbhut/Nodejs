427034a31abb44e7b45b60cdab9445cd2f7c5499
DnyaneshKalbhut <kalbhutdnyanesh@gmail.com>
2024-03-12 15:23:14 +0530

    File handling in Node

---
+++ 02-FileHandling/file.js
@@ -0,0 +1,36 @@
+
+const fs = require("fs")
+
+
+// Synchronus 
+// fs.writeFileSync("./test.txt","hello there dnyaesh");
+
+// Asynchronous
+// fs.writeFile("./text.txt","from ASync filewriter",(error)=>{})
+
+
+// const result= fs.readFileSync("./contact.txt","utf-8");
+
+// console.log(result);
+
+
+// Sync will return value in variable
+//but Asynchronous always need an callback function to return a result
+
+
+// fs.readFile("./contact.txt","utf-8",(err,result)=>{
+//     if (err) {
+//         console.log(err);
+//     }else{
+//         console.log(result);
+//     }
+// })
+
+
+fs.appendFileSync("./test.txt",`hello there \n ${Date.now()}`);
+
+fs.cpSync("./test.txt","./copy.txt");
+
+fs.unlinkSync("./copy.txt")
+
+console.log(fs.statSync("./text.txt"));
