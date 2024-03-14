5dc6d338b11e23f4aa22a31437d381819042ad4a
DnyaneshKalbhut <kalbhutdnyanesh@gmail.com>
2024-03-14 14:43:36 +0530

    url handling

--- 03-Server/index.js
+++ 03-Server/index.js
@@ -1,15 +1,22 @@
 const http = require("http");
 const fs = require("fs");
+const url= require("url")
 
 const myServer = http.createServer((req, res) => {
+  if(req.url==="/favicon.ico") return res.end();
+ 
   const log = `${Date.now()} ${req.url} New req recieved \n`;
+  const myUrl=url.parse(req.url,true);
+console.log(myUrl);
+
   fs.appendFile("log.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
         res.end("HOMEPAGE");
         break;
       case "/about":
-        res.end("Hello it s me DK");
+        const username = myUrl.query.myname;
+        res.end(`hi ${username}`);
         break;
       default:
         res.end("404 not ffound");
