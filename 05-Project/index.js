de91bca196f42c81435e98e3c2393e962abb9499
DnyaneshKalbhut <kalbhutdnyanesh@gmail.com>
2024-04-01 12:36:17 +0530

    user created

--- 05-Project/index.js
+++ 05-Project/index.js
@@ -1,48 +1,51 @@
 const express = require("express");
-const users = require("./MOCK_DATA.json")
-
+const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 const app = express();
 
-const PORT=8000;
-// app.get("/users",(req,res)=>{
-//     const html=`
-//     <ul>
-//     ${users.map((user)=> `<li>${user.first_name} </li>`).join("")};
-//     </ul>
-//     `
-//     res.send(html);
-// })
+const PORT = 8000;
+app.use(express.urlencoded({ extended: false }));
 
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+    ${users.map((user) => `<li>${user.first_name} </li>`).join("")};
+    </ul>
+    `;
+  res.send(html);
+});
 
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
 
-app.get("/api/users",(req,res)=>{
-    return res.json(users);
+//id - dynamic path parameter
+app.get("/api/users/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    const user= users.find((user)=>user.id==id);
+
+    return res.json(user);
 })
 
-
-// :id - dynamic path parameter
-// app.get("/api/users/:id",(req,res)=>{
-//     const id = Number(req.params.id);
-//     const user= users.find((user)=>user.id==id);
-
-//     return res.json(user);
-// })
-
-// app.post('/api/users',(req,res)=>{
-
-//     return res.json({status:"pending"});
-// })
-// app.patch('/api/users/:id',(req,res)=>{
-
-//     return res.json({status:"pending"});
-// });
-// app.delete('/api/users/:id',(req,res)=>{
-
-//     return res.json({status:"pending"});
-// })
-
-app.route("/api/users/:id").get().patch().delete()
-
-app.listen(PORT,()=>{
-    console.log(`server started at PORT: ${PORT}`);
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "sucess",id: users.length + 1 });
+  });
+  console.log(body);
+});
+app.patch('/api/users/:id',(req,res)=>{
+      
+    return res.json({status:"pending"});
+});
+app.delete('/api/users/:id',(req,res)=>{
+    
+    return res.json({status:"pending"});
 })
+
+app.route("/api/users/:id").get().patch().delete();
+
+app.listen(PORT, () => {
+  console.log(`server started at PORT: ${PORT}`);
+});
